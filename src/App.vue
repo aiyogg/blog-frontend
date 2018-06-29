cc256b11a3816e12d18e46befd0a862325a941f2
Chuck <tengchenc@gmail.com>
2018-06-30 00:10:46 +0800

    style: modify some css

--- src/App.vue
+++ src/App.vue
@@ -135,7 +135,7 @@
     min-height: 100%;
     width: 100%;
     box-sizing: border-box;
-    font-family: $font-family-sans-serif !important;
+    font-family: $font-family-heiti !important;
     opacity: 1;
     /*transition: all ease 500ms;*/
 
